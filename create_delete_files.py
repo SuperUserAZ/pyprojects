f77fe314d21ce42235e7f40cace1215575e8e6ab
super0user <88291146+super0user@users.noreply.github.com>
2021-08-23 11:26:59 +0400

    Update create_delete_files.py
    just fix

--- create_delete_files.py
+++ create_delete_files.py
@@ -5,21 +5,23 @@ import argparse
 parse = argparse.ArgumentParser()
 parse.add_argument('-fo', '--folder', type=str, help = 'creates a new folder or navigates to that folder')
 parse.add_argument('-f', '--file', type=str, help='the name of the instance of the file or files')
-parse.add_argument('-df', '--delfolder', type=str, help='deletes folder')
 parse.add_argument('-cr', '--create',type = bool, help='creates a file')
 parse.add_argument('-d', '--delete', type= bool, help='deletes the file')
 parse.add_argument('-co', '--count', type = int, help='indicates the number of instances')
 args = parse.parse_args()
 
 folder_name = args.folder
-del_folder_name = args.delfolder
 file_name = args.file
 count = args.count
 point_pos = file_name.find('.') if file_name else None
 count = (count +1) if count else None
 
-path_dir = f'{os.getcwd()}\\{folder_name}' if folder_name else os.getcwd()
-dirs = os.listdir(path_dir)
+path_list = os.listdir()
+path_dir = os.getcwd() 
+
+if folder_name:
+    path_dir = f'{os.getcwd()}\\{folder_name}'
+    
 
 created_list = list()
 remove_list = list()
@@ -42,56 +44,63 @@ def delete_folder(folder_name):
 # the function creates files in the current directory, the number of which does not exceed 'count'
 # if there are these files in the current directory, then the function does not duplicate them, 
 # but only adds the missing amount
-def create_js_file(tmp):    
+def create_file(tmp):    
     for k in range(1, count):
         tmp = file_name[:point_pos] + f'{k}' + file_name[point_pos:]
-        if  tmp not in dirs:
-            new_js_file = os.path.join(path_dir, tmp)
-            fl = open(new_js_file, 'w')
-            fl.close()
+        if  os.path.exists(path_dir):
+            new_file = os.path.join(path_dir, tmp)
+            fl = open(new_file, 'w').close()
             created_list.append(tmp)
         else:
             exists_list.append(tmp)
 
 # a function that removes files that enter the loop from zero to 'count'
 # if the file is not in the current directory, then the function checks the next by number
-def delete_js_file(tmp):
+def delete_file(tmp):
     for j in range(1, count):
         tmp = file_name[:point_pos] + f'{j}' + file_name[point_pos:]
-        if tmp in dirs:
-            old_js_file = os.path.join(path_dir, tmp)
+        if os.path.exists(f'{path_dir}\\{tmp}'):
+            old_file = os.path.join(path_dir, tmp)
             remove_list.append(tmp)
-            os.remove(old_js_file)
+            os.remove(old_file)
         else:
             exists_list.append(tmp)
 
-def create(fl_name, fol_name, cr_ls, ex_ls):
-    if fol_name:
-        create_folder(folder_name)
-    create_js_file(fl_name)
+def create(f_name, cr_ls):
+    create_file(f_name)
     created_list = json.dumps(cr_ls)
-    exists_list = json.dumps(ex_ls)
+    #exists_list = json.dumps(ex_ls)
     jsoncreated = json.loads(created_list)
-    jsonexists = json.loads(exists_list)
+    #jsonexists = json.loads(exists_list)
     print('The following files have been successfully ',
-    'created in the current directory:\n {}'.format(jsoncreated))
-    print('The following files exist in the current directory: \n{}'.format(jsonexists))
+    'created in the current directory: \n({})\n {}'.format(path_dir, jsoncreated))
+    #print('The following files exist in the current directory: \n{}'.format(jsonexists))
 
-def delete(fl_name, cr_ls, ex_ls):
-        delete_js_file(fl_name)
-        deleted_list = json.dumps(remove_list)
-        NOexists_list = json.dumps(exists_list)
-        jsondeleted = json.loads(deleted_list)
-        jsonNOexists = json.loads(NOexists_list)
-        print('The following files have been successfully',
-        'removed from the current directory:\n {}'.format(jsondeleted))
-        print('The following files not exist in the current directory: \n{}'.format(jsonNOexists))
-        
-if __name__ == '__main__':
-    if args.create and not args.delete:
-        create(file_name, folder_name, created_list, exists_list)
-    elif args.delete and not args.create:
-        delete(file_name, created_list, exists_list)
-    elif del_folder_name:
-        delete_folder(del_folder_name)
-        print(del_folder_name)
+def delete(f_name, rm_ls):
+    delete_file(f_name)
+    deleted_list = json.dumps(rm_ls)
+    #NOexists_list = json.dumps(ex_ls)
+    jsondeleted = json.loads(deleted_list)
+    #jsonNOexists = json.loads(NOexists_list)
+    print('The following files have been successfully',
+    'removed from the current directory: \n({})\n {}'.format(path_dir, jsondeleted))
+    #print('The following files not exist in the current directory: \n{}'.format(jsonNOexists))
+    
+if __name__ == '__main__': 
+    if folder_name and args.create:
+        create_folder(folder_name)
+        if file_name:
+            create(file_name, created_list)    
+    elif not folder_name and args.create:
+        create(file_name, created_list)
+    elif not folder_name and args.delete:
+        delete(file_name, remove_list)
+    elif folder_name and args.delete:
+        if file_name:
+            delete(file_name,remove_list)
+            os.chdir(path_dir)
+        tmp_ls = os.listdir()
+        os.chdir(r'C:\Users\Shadow\Desktop\py_projects')
+        if not tmp_ls:
+            print(tmp_ls)
+            delete_folder(folder_name
