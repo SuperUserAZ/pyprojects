d2d65b7bd4cd6c30aa1a06568f174ccbf25c9015
super0user <88291146+super0user@users.noreply.github.com>
2021-10-25 23:53:33 +0400

    Create rearrang_list.py

---
+++ other/rearrang_list.py
@@ -0,0 +1,37 @@
+if __name__ == '__main__':
+    N = int(input())
+    input_list = list()
+    command_list = list()
+    command, params = list(), list()
+
+    for i in range(N):
+        obj = list(input().split(' '))
+        com = obj[0]
+        result1 = [int(ob) for ob in obj[1:]]
+        params.append(result1)
+        command.append(com)
+        result2 = tuple(zip(command, params))
+
+    command_list.extend(result2)
+
+    for _, command in enumerate(command_list):
+        com1 = command[0]
+        com2 = None
+        if command[1]:
+            com2 = command[1][0]
+        if com1 == 'insert':
+            input_list.insert(com2, command[1][1])
+        elif com1 == 'append':
+            input_list.append(com2)
+        elif com1 == 'remove':
+            if com2 in input_list:
+                input_list.remove(com2)
+        elif com1 == 'pop':
+            if input_list:
+                input_list.pop()
+        elif com1 == 'sort':
+            input_list.sort()
+        elif com1 == 'reverse':
+            input_list = list(reversed(input_list))
+        elif com1 == 'print':
+            print(input_list)
